4e4d011c8c3dce007d82047aaa772bb95aada381
mityaycho <mityaycho@gmail.com>
2019-10-17 19:14:05 +0300

    add import {NavLink} from 'react-router-dom';

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import iconCachedWhite from '../images/icon_cached_white.png';
+import ContainerFormOneTwoPage from "./ContainerFormOneTwoPage";
 
 const SectionHeader = () => {
 	return (
@@ -10,24 +10,11 @@ const SectionHeader = () => {
 					<h1 className="display-4">Вся жизнь -</h1>
 					<h1 className="display-4 font-weight-bold">путешествие!</h1>
 				</div>
-				<div className="section-header-form d-flex flex-column p-3">
-					<div className="row mt-5">
-						<form className="form mt-5 w-100" action="input">
-							<p className="ml-3">Направление</p>
-							<div className="d-flex form-group m-3">
-								<input className="col-sm form-control" type="text" placeholder="откуда" />
-								<img className="mt-auto mb-2" src={iconCachedWhite} alt="..."/>
-								<input className="col-sm form-control mr-2" type="text" placeholder="куда" />
-							</div>
-							<div className="d-flex w-100 h-100">
-								<div className="d-flex w-100 form-group m-3">
-									<input className="col-sm mr-4 form-control" type="date" />
-									<input className="col-sm mr-2 form-control" type="date" />
-								</div>
-							</div>
-						</form>
+				<div className="section-header-form col-lg-6">
+					<ContainerFormOneTwoPage />
+					<div className="text-right ml-4">
+						<NavLink className="btn btn-warning mt-5 col-lg-6" to="/two_page" type="button">найти билеты</NavLink>
 					</div>
-					<NavLink className="btn btn-warning btn-sx btn-block mb-4 mt-auto w-50 align-self-end" to="/two_page" type="button">найти билеты</NavLink>
 				</div>
 			</div>
 		</div>
--- src/components/SectionHeaderTwoPage.jsx
+++ src/components/SectionHeaderTwoPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import iconCachedWhite from '../images/icon_cached_white.png';
+import ContainerFormOneTwoPage from "./ContainerFormOneTwoPage";
 
 const SectionHeaderTwoPage = () => {
 	return (
@@ -11,23 +11,7 @@ const SectionHeaderTwoPage = () => {
 					<h1 className="display-4 font-weight-bold">путешествие!</h1>
 				</div>
 				<div className="section-header-form col-lg-6">
-					<div className="row mt-5">
-						<form className="form mt-5 w-100" action="input">
-							<p className="ml-3">Направление</p>
-							<div className="d-flex form-group m-3">
-								<input className="col-sm form-control" type="text" placeholder="откуда" />
-								<img className="mt-auto mb-2" src={iconCachedWhite} alt="..."/>
-								<input className="col-sm form-control" type="text" placeholder="куда" />
-							</div>
-						</form>
-						<form className="form mt-5 w-100" action="input">
-							<p className="ml-3">Дата</p>
-							<div className="d-flex form-group m-3">
-								<input className="col-sm form-control mr-4" type="date" placeholder="дд/мм/гг" />
-								<input className="col-sm form-control" type="date" placeholder="дд/мм/гг" />
-							</div>
-						</form>
-					</div>
+					<ContainerFormOneTwoPage />
 					<div className="text-right ml-4">
 						<NavLink className="btn btn-warning mt-5 col-lg-6" to="/three_page" type="button">найти билеты</NavLink>
 					</div>
