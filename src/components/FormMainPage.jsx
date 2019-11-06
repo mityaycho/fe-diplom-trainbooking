2d5ddb7358b6b477904edbf00e5ac8b97b690aaf
mityaycho <mityaycho@gmail.com>
2019-11-07 01:12:43 +0300

    add onInputChange in Typeahead

--- src/components/FormMainPage.jsx
+++ src/components/FormMainPage.jsx
@@ -5,7 +5,6 @@ import {Typeahead} from 'react-bootstrap-typeahead';
 
 
 const FormMainPage = (props) => {
-  const dataCities = props.dataCities.map(el => <option key={el.id}>{el.name}</option>);
   const setSearchItem = (e) => props.setSearchItem(e);
   const options = props.dataCities.map(el => el.name);
 
