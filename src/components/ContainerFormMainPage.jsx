c885b0bc9f6de5c2483039e970ac9ff915c2b8b2
mityaycho <mityaycho@gmail.com>
2019-11-09 17:54:53 +0300

    add setWhereFromDate function

--- src/components/ContainerFormMainPage.jsx
+++ src/components/ContainerFormMainPage.jsx
@@ -5,22 +5,55 @@ import FormMainPage from "./FormMainPage";
 
 
 class ContainerFormMainPage extends React.Component {
+
+  state = {
+    value: '',
+    dataCities: [],
+    whereFromName: '',
+    whereFromDate: null,
+    whereToName: '',
+    whereToDate: null
+  };
+
   componentDidMount() {
-    fetch(`https://netology-trainbooking.herokuapp.com/routes/cities?name=c`)
+    fetch(`https://netology-trainbooking.herokuapp.com/routes/cities?name=с`)
       .then(response => response.json())
-      .then(data => this.props.setDataCities(data));
+      .then(data => this.setState({dataCities: data}));
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.props.searchItem !== prevProps.searchItem) {
-      fetch(`https://netology-trainbooking.herokuapp.com/routes/cities?name=${this.props.searchItem}`)
+    if (this.state.value !== prevProps.value) {
+      fetch(`https://netology-trainbooking.herokuapp.com/routes/cities?name=${this.state.value}`)
         .then(response => response.json())
-        .then(data => this.props.setDataCities(data));
+        .then(data => this.setState(data.error ? {dataCities: [], value: data.error} : {dataCities: data}));
     };
   };
 
+  setEvent = (event) => {
+    this.setState({value: event})
+  };
+
+  setWhereFromName = (event) => {
+    this.setState({whereFromName: event[0]})
+  };
+
+  setWhereToName = (event) => {
+    this.setState({whereToName: event[0]})
+  };
+
+  setWhereFromDate = (event) => {
+    this.setState({whereFromDate: event.currentTarget.value})
+  };
+
+
+
   render() {
-    return <FormMainPage {...this.props}/>;
+    return <FormMainPage {...this.state}
+                         setEvent={this.setEvent}
+                         setWhereFromName={this.setWhereFromName}
+                         setWhereToName={this.setWhereToName}
+                         setWhereFromDate={this.setWhereFromDate}
+    />;
   };
 };
 
--- src/components/FormMainPage.jsx
+++ src/components/FormMainPage.jsx
@@ -1,54 +1,50 @@
 import React, {Fragment} from 'react';
-import iconCachedWhite from "../images/icon_cached_white.png";
+import iconCachedWhite from '../images/icon_cached_white.png';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import {Typeahead} from 'react-bootstrap-typeahead';
 
 
-class FormMainPage extends React.Component {
+const FormMainPage = (props) => {
+  console.log(props)
 
-  state = {
+  const options = props.dataCities.map(el => el.name);
 
-  }
-
-  setSearchItem = (e) => {
-    this.props.setSearchItem(e);
-  }
-  options = this.props.dataCities.map(el => el.name);
-
-  render() {
     return (
       <div className="row mt-5">
         <form className="form mt-5 w-100" action="input">
           <p className="ml-3">Направление</p>
           <div className="d-flex form-group m-3 justify-content-center">
             <Fragment>
-              <Typeahead id={1} value={this.props.searchItem}
-                         placeholder={this.props.error ? this.props.error : "откуда"}
-                         options={this.options}
-                         onInputChange={this.setSearchItem}
-                         onChange={(selected) => console.log(selected[0])}
+              <Typeahead value={props.value}
+                         placeholder={props.dataCities.error ? props.dataCities.error : "откуда"}
+                         options={options}
+                         onInputChange={props.setEvent}
+                         onChange={props.setWhereFromName}
               />
             </Fragment>
             <img className="mt-auto mb-2" src={iconCachedWhite} alt="..."/>
 
-            <Typeahead
-              value={this.props.searchItem}
-              placeholder={this.props.error ? this.props.error : "куда"}
-              options={this.options}
-              onInputChange={this.setSearchItem}
+             <Typeahead
+              value={props.searchItem}
+              placeholder={props.dataCities.error ? props.dataCities.error : "куда"}
+              options={options}
+              onInputChange={props.setSearchItem}
+              onChange={props.setWhereToName}
             />
           </div>
         </form>
         <form className="form mt-5 w-100" action="input">
           <p className="ml-3">Дата</p>
           <div className="d-flex form-group m-3">
-            <input className="col-sm form-control mr-4" type="date" onChange={(e) => console.log(e.currentTarget.value)}/>
-            <input className="col-sm form-control" type="date" />
+            <input className="col-sm form-control mr-4"
+                   type="date"
+                   onChange={props.setWhereFromDate} />
+            <input className="col-sm form-control"
+                   type="date" />
           </div>
         </form>
       </div>
     );
-  };
 };
 
 
