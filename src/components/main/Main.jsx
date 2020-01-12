3fc9bda3f436a2104eb28869da6840613c3a2780
mityaycho <mityaycho@gmail.com>
2020-01-12 23:42:57 +0300

    add some changes

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import AboutUs from './components/AboutUs';
 import HowItWorks from './components/HowItWorks';
 import Reviews from './components/Reviews';
-import ContainerFormMainPage from "./components/ContainerFormMainPage";
+import ContainerFormMainPage from './components/ContainerFormMainPage';
 
 class Main extends Component {
 	render() {
