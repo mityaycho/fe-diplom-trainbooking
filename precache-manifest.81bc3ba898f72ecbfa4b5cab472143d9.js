2588b6ae4330569037a18e1db63a38139acd7cb7
mityaycho <mityaycho@gmail.com>
2020-02-14 17:06:02 +0300

    Updates

---
+++ precache-manifest.81bc3ba898f72ecbfa4b5cab472143d9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "be2f5b8cf9d4d67e398b",
+    "url": "/fe-diplom-trainbooking/static/css/main.97a37ffd.chunk.css"
+  },
+  {
+    "revision": "be2f5b8cf9d4d67e398b",
+    "url": "/fe-diplom-trainbooking/static/js/main.9cc91d2e.chunk.js"
+  },
+  {
+    "revision": "abeac4f788226cbc4347",
+    "url": "/fe-diplom-trainbooking/static/js/runtime~main.1e9ecbb8.js"
+  },
+  {
+    "revision": "940ba0d149e1f9b23068",
+    "url": "/fe-diplom-trainbooking/static/css/2.1b6116bd.chunk.css"
+  },
+  {
+    "revision": "940ba0d149e1f9b23068",
+    "url": "/fe-diplom-trainbooking/static/js/2.32c3e5ae.chunk.js"
+  },
+  {
+    "revision": "0967c085eccf041ba024db11f9022dd1",
+    "url": "/fe-diplom-trainbooking/static/media/image_vagon_places.0967c085.png"
+  },
+  {
+    "revision": "2f0f923c223074af6f5e8ce0d3088e65",
+    "url": "/fe-diplom-trainbooking/static/media/review_icon2.2f0f923c.png"
+  },
+  {
+    "revision": "b4afe6045b31c3c772be38de257f6009",
+    "url": "/fe-diplom-trainbooking/static/media/review_icon1.b4afe604.png"
+  },
+  {
+    "revision": "2b92ad877f645b5ec726848ff3212e37",
+    "url": "/fe-diplom-trainbooking/static/media/image_logo2.2b92ad87.png"
+  },
+  {
+    "revision": "23451f57d623800b4d18c8f9a1965fcd",
+    "url": "/fe-diplom-trainbooking/static/media/image_logo1.23451f57.png"
+  },
+  {
+    "revision": "0f5d5b56b60eade81ec45d2ec3b3abab",
+    "url": "/fe-diplom-trainbooking/static/media/mask_group.0f5d5b56.png"
+  },
+  {
+    "revision": "4ff73f970d002d81419c0cd6224a4f5e",
+    "url": "/fe-diplom-trainbooking/static/media/image_logo_final_order.4ff73f97.png"
+  },
+  {
+    "revision": "7e20441827e2ac094a257ebb72f7d908",
+    "url": "/fe-diplom-trainbooking/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.684da9458942de4dfbf8bff91ff8357c.js"
+  "/fe-diplom-trainbooking/precache-manifest.81bc3ba898f72ecbfa4b5cab472143d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fe-diplom-trainbooking/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
