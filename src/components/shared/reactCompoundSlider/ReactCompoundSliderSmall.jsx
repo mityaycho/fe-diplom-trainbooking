9421bb7f7fd99c9878f6f1a30d2eb229e0cb7687
mityaycho <mityaycho@gmail.com>
2020-04-07 22:15:18 +0300

    add small slider

---
+++ src/components/shared/reactCompoundSlider/ReactCompoundSliderSmall.jsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider';
+import { Handle, Track, Tick } from './components/componentsSliderSmall';
+
+const sliderStyle = {
+  margin: '0%',
+	position: 'relative',
+	width: '100%'
+};
+
+const railStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: 16,
+  borderRadius: 7,
+  cursor: 'pointer',
+	backgroundColor: '#3E3C41',
+	border: '1px solid white'
+};
+
+const domain = [0, 5000];
+
+class ReactCompoundSlider extends React.Component {
+  state = {
+    values: [0, 5000]
+  };
+
+  onChange = (values) => {
+		this.setState({ values });
+		console.log(this.state.values);
+  };
+
+  render() {
+    const {
+      state: { values }
+    } = this;
+
+    return (
+      <div style={{ height: 15, width: '100%' }}>
+        <Slider
+          mode={1}
+          step={1}
+          domain={domain}
+          rootStyle={sliderStyle}
+          onChange={this.onChange}
+          values={values}
+        >
+          <Rail>
+            {({ getRailProps }) => (
+              <div style={railStyle} {...getRailProps()} />
+            )}
+          </Rail>
+          <Handles>
+            {({ handles, getHandleProps }) => (
+              <div className="slider-handles">
+                {handles.map(handle => (
+                  <Handle
+                    key={handle.id}
+                    handle={handle}
+                    domain={domain}
+                    getHandleProps={getHandleProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Handles>
+          <Tracks left={false} right={false}>
+            {({ tracks, getTrackProps }) => (
+              <div className="slider-tracks">
+                {tracks.map(({ id, source, target }) => (
+                  <Track
+                    key={id}
+                    source={source}
+                    target={target}
+                    getTrackProps={getTrackProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Tracks>
+          <Ticks count={1}>
+            {({ ticks }) => (
+              <div className="slider-ticks">
+                {ticks.map(tick => (
+                  <Tick key={tick.id} tick={tick} count={ticks.length} />
+                ))}
+              </div>
+            )}
+          </Ticks>
+        </Slider>
+      </div>
+    );
+  };
+};
+
+export default ReactCompoundSlider;
---
+++ src/components/shared/reactCompoundSlider/components/componentsSliderSmall.jsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import {
+  SliderItem,
+  GetHandleProps,
+  GetTrackProps
+} from 'react-compound-slider';
+
+// *******************************************************
+// HANDLE COMPONENT
+// *******************************************************
+// interface IHandleProps {
+//   domain: number[];
+//   handle: SliderItem;
+//   getHandleProps: GetHandleProps;
+// }
+
+export const Handle = ({
+  domain: [min, max],
+  handle: { id, value, percent },
+  getHandleProps
+}) => (
+  <div
+    role="slider"
+    aria-valuemin={min}
+    aria-valuemax={max}
+    aria-valuenow={value}
+    style={{
+      left: `${percent}%`,
+      position: 'absolute',
+      marginLeft: '-9px',
+      marginTop: '-1px',
+      zIndex: 2,
+      width: 18,
+      height: 18,
+      cursor: 'pointer',
+      borderRadius: '50%',
+      boxShadow: '1px 1px 1px 1px rgba(0, 0, 0, 0.2)',
+      backgroundColor: '#ffff'
+    }}
+    {...getHandleProps(id)}
+  />
+);
+
+// *******************************************************
+// TRACK COMPONENT
+// *******************************************************
+// interface ITrackProps {
+//   source: SliderItem;
+//   target: SliderItem;
+//   getTrackProps: GetTrackProps;
+// }
+
+export const Track = ({
+  source,
+  target,
+  getTrackProps
+}) => (
+  <div
+    style={{
+      position: 'absolute',
+      height: 16,
+      zIndex: 1,
+      backgroundColor: '#FFA800',
+      borderRadius: 7,
+      cursor: 'pointer',
+      left: `${source.percent}%`,
+      width: `${target.percent - source.percent}%`
+    }}
+    {...getTrackProps()}
+  />
+);
+
+// *******************************************************
+// TICK COMPONENT
+// *******************************************************
+// interface ITickProps {
+//   key: string;
+//   tick: SliderItem;
+//   count: number;
+// }
+
+export const Tick = ({ tick, count }) => (
+  <div>
+    <div
+      style={{
+        position: 'absolute',
+        marginTop: 10,
+        width: 1,
+        height: 0,
+        backgroundColor: 'rgb(200,200,200)',
+        left: `${tick.percent}%`
+      }}
+    />
+    <div
+      style={{
+        position: 'absolute',
+        marginTop: 18,
+        fontSize: 14,
+        textAlign: 'center',
+        marginLeft: `${-(100 / count) / 2}%`,
+        width: `${100 / count}%`,
+				left: `${tick.percent}%`
+      }}
+    >
+      {tick.value}
+    </div>
+  </div>
+);
