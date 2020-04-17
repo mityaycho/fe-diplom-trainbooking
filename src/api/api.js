f3f498b42b06d7b367fd1cd1bbb4a58e6255a804
mityaycho <mityaycho@gmail.com>
2020-04-17 23:03:13 +0300

    add some changes

--- src/api/api.js
+++ src/api/api.js
@@ -9,8 +9,8 @@ export const api = {
 		return instance.get('routes/last')
 	},
 	filterRoutes(
-		cityWhereFromId, 
-		cityWhereToId, 
+		cityWhereFromId,
+		cityWhereToId,
 		sort,
 		limit,
 		offset,
@@ -30,32 +30,32 @@ export const api = {
 		end_departure_hour_to,
 		end_arrival_hour_from,
 		end_arrival_hour_to
-		) {
+	) {
 		// cityWhereFromId: "5b9a2fa7f83e028786ea5672"
 		// cityWhereToId: "5b9a2fa7f83e028786ea5673"
 		// `routes?from_city_id=${cityWhereFromId}&to_city_id=${cityWhereToId}` (line 28)
 
 		return instance.get(
-				`routes?from_city_id=${'5b9a2fa7f83e028786ea5672'}&to_city_id=${'5b9a2fa7f83e028786ea5673'}` +
-				(`${sort}` ? `&sort=${sort}` : '') +
-				(`${limit}` ? `&limit=${limit}` : '') +
-				(`${offset}` ? `&offset=${offset}` : '') +
-				`${have_second_class ? '&have_second_class=true' : ''}` +
-				`${have_third_class ? '&have_third_class=true' : ''}` +
-				`${have_fourth_class ? '&have_fourth_class=true' : ''}` +
-				`${have_first_class ? '&have_first_class=true' : ''}` +
-				`${have_wifi ? '&have_wifi=true' : ''}` +
-				`${have_express ? '&have_express=true' : ''}` +
-				`${price_from ? `&price_from=${price_from}` : ''}` +
-				`${price_to ? `&price_to=${price_to}` : ''}` +
-				`${start_departure_hour_from ? `&start_departure_hour_from=${start_departure_hour_from}` : ''}` +
-				`${start_departure_hour_to ? `&start_departure_hour_to=${start_departure_hour_to}` : ''}` +
-				`${start_arrival_hour_from ? `&start_arrival_hour_from=${start_arrival_hour_from}` : ''}` +
-				`${start_arrival_hour_to ? `&start_arrival_hour_to=${start_arrival_hour_to}` : ''}` +
-				`${end_departure_hour_from ? `&end_departure_hour_from=${end_departure_hour_from}` : ''}` +
-				`${end_departure_hour_to ? `&end_departure_hour_to=${end_departure_hour_to}` : ''}` +
-				`${end_arrival_hour_from ? `&end_arrival_hour_from=${end_arrival_hour_from}` : ''}` +
-				`${end_arrival_hour_to ? `&end_arrival_hour_to=${end_arrival_hour_to}` : ''}`
-				)
+			`routes?from_city_id=${'5b9a2fa7f83e028786ea5672'}&to_city_id=${'5b9a2fa7f83e028786ea5673'}` +
+			(`${sort}` ? `&sort=${sort}` : '') +
+			(`${limit}` ? `&limit=${limit}` : '') +
+			(`${offset}` ? `&offset=${offset}` : '') +
+			`${have_second_class ? '&have_second_class=true' : ''}` +
+			`${have_third_class ? '&have_third_class=true' : ''}` +
+			`${have_fourth_class ? '&have_fourth_class=true' : ''}` +
+			`${have_first_class ? '&have_first_class=true' : ''}` +
+			`${have_wifi ? '&have_wifi=true' : ''}` +
+			`${have_express ? '&have_express=true' : ''}` +
+			`${price_from ? `&price_from=${price_from}` : ''}` +
+			`${price_to ? `&price_to=${price_to}` : ''}` +
+			`${start_departure_hour_from ? `&start_departure_hour_from=${start_departure_hour_from}` : ''}` +
+			`${start_departure_hour_to ? `&start_departure_hour_to=${start_departure_hour_to}` : ''}` +
+			`${start_arrival_hour_from ? `&start_arrival_hour_from=${start_arrival_hour_from}` : ''}` +
+			`${start_arrival_hour_to ? `&start_arrival_hour_to=${start_arrival_hour_to}` : ''}` +
+			`${end_departure_hour_from ? `&end_departure_hour_from=${end_departure_hour_from}` : ''}` +
+			`${end_departure_hour_to ? `&end_departure_hour_to=${end_departure_hour_to}` : ''}` +
+			`${end_arrival_hour_from ? `&end_arrival_hour_from=${end_arrival_hour_from}` : ''}` +
+			`${end_arrival_hour_to ? `&end_arrival_hour_to=${end_arrival_hour_to}` : ''}`
+		)
 	}
 };
--- src/components/searchTickets/components/SearchTicketsJSX.jsx
+++ src/components/searchTickets/components/SearchTicketsJSX.jsx
@@ -36,8 +36,8 @@ class SearchTicketsJSX extends React.Component {
 	};
 
 	componentDidUpdate(prevProps, prevState) {
-		if (prevState.sort !== this.state.sort || 
-			prevState.limit !== this.state.limit || 
+		if (prevState.sort !== this.state.sort ||
+			prevState.limit !== this.state.limit ||
 			prevState.offset !== this.state.offset) {
 			this.getTicketsFetch();
 		};
@@ -48,123 +48,124 @@ class SearchTicketsJSX extends React.Component {
 	filterChoiceTickets = (event) => this.setState({
 		limit: event.currentTarget.innerHTML,
 		offset: 0,
-		nextPageDisabled: false });
+		nextPageDisabled: false
+	});
 
 	setPrevPageOffset = () => {
 		if (this.state.offset / Number(this.state.limit) === 1) {
-			this.setState({prevPageDisabled: true});
+			this.setState({ prevPageDisabled: true });
 		}
 		this.setState({
-			offset: (Number(this.state.offset) - Number(this.state.limit)), 
-			nextPageDisabled: false});
+			offset: (Number(this.state.offset) - Number(this.state.limit)),
+			nextPageDisabled: false
+		});
 	};
 
 	setNextPageOffset = () => {
 		let pages = Math.ceil(Number(this.props.total_count) / Number(this.state.limit));
 		if (pages === (this.state.offset / Number(this.state.limit)) + 2) {
-			this.setState({nextPageDisabled: true});
+			this.setState({ nextPageDisabled: true });
 		}
 		if (pages >= (this.state.offset / Number(this.state.limit)) - 2) {
-			this.setState({prevPageDisabled: false});
+			this.setState({ prevPageDisabled: false });
 		}
-		this.setState({offset: Number(this.state.offset) + Number(this.state.limit)});
+		this.setState({ offset: Number(this.state.offset) + Number(this.state.limit) });
 	};
 
 	setButtonOffset = (event) => {
-		this.setState({offset: (Number(this.state.limit) * Number(event.currentTarget.innerHTML)) - Number(this.state.limit)});
+		this.setState({ offset: (Number(this.state.limit) * Number(event.currentTarget.innerHTML)) - Number(this.state.limit) });
 		let pages = Math.ceil(Number(this.props.total_count) / Number(this.state.limit));
-	
+
 		if (pages === Number(event.currentTarget.innerHTML)) {
 			this.setState({
-					nextPageDisabled: true,
-					prevPageDisabled: false
+				nextPageDisabled: true,
+				prevPageDisabled: false
 			});
-	} else if (Number(event.currentTarget.innerHTML) === 1 ) {
+		} else if (Number(event.currentTarget.innerHTML) === 1) {
 			this.setState({
-					prevPageDisabled: true,
-					nextPageDisabled: false,
+				prevPageDisabled: true,
+				nextPageDisabled: false,
 			});
-	} else {
+		} else {
 			this.setState({
-					nextPageDisabled: false,
-					prevPageDisabled: false
+				nextPageDisabled: false,
+				prevPageDisabled: false
 			});
-	}
-
+		}
 	}
 
 	render() {
 
-	let classFilterChoiceFive = this.state.limit === "5" ? "filter-choice-tickets-active" : "filter-choice-tickets";
-	let classFilterChoiceTen = this.state.limit === "10" ? "filter-choice-tickets-active" : "filter-choice-tickets";
-	let classFilterChoiceTwenty = this.state.limit === "20" ? "filter-choice-tickets-active" : "filter-choice-tickets";
+		let classFilterChoiceFive = this.state.limit === "5" ? "filter-choice-tickets-active" : "filter-choice-tickets";
+		let classFilterChoiceTen = this.state.limit === "10" ? "filter-choice-tickets-active" : "filter-choice-tickets";
+		let classFilterChoiceTwenty = this.state.limit === "20" ? "filter-choice-tickets-active" : "filter-choice-tickets";
 
 		let pages = Math.ceil(Number(this.props.total_count) / Number(this.state.limit));
-	let buttonsPages = [];
+		let buttonsPages = [];
 
-	for (let i = 1; i <= pages; i++) {
-		const cls = ['page-search-select-number', 'ml-3'];
-		if ((this.state.offset / Number(this.state.limit)) + 1 === i) {
-			cls.push('active');
+		for (let i = 1; i <= pages; i++) {
+			const cls = ['page-search-select-number', 'ml-3'];
+			if ((this.state.offset / Number(this.state.limit)) + 1 === i) {
+				cls.push('active');
+			}
+			buttonsPages.push(<button className={cls.join(' ')}
+				key={i}
+				type="button"
+				onClick={this.setButtonOffset}>{i}</button>);
 		}
-		buttonsPages.push(<button className={cls.join(' ')} 
-		key={i} 
-		type="button" 
-		onClick={this.setButtonOffset}>{i}</button>);
-	}
 
-	const resultSearchTicketsJSX = this.props.items ? this.props.items.map((el, idx) =>
-		<ResultSearchTickets key={idx} state={el} />) : [];
+		const resultSearchTicketsJSX = this.props.items ? this.props.items.map((el, idx) =>
+			<ResultSearchTickets key={idx} state={el} />) : [];
 
-	return (
-		<div className="tickets-search-result col-lg-9 pt-5 pb-5 pl-5">
-			<div className="row">
-				<div className="col">найдено {this.props.total_count}</div>
-				<div className="row text-right">сортировать по:&nbsp;
-					<select className="custom-sort-train" 
-								name="sortTrain" 
-								id="sort" 
-								onChange={this.sortSearch}
-								value={this.state.sort}>
-						<option value="date">времени</option>
-						<option value="price">стоимости</option>
-						<option value="duration">длительности</option>
-					</select>
-				</div>
-				<div className="row text-right ml-5 mr-3">показывать по:&nbsp;
+		return (
+			<div className="tickets-search-result col-lg-9 pt-5 pb-5 pl-5">
+				<div className="row">
+					<div className="col">найдено {this.props.total_count}</div>
+					<div className="row text-right">сортировать по:&nbsp;
+					<select className="custom-sort-train"
+							name="sortTrain"
+							id="sort"
+							onChange={this.sortSearch}
+							value={this.state.sort}>
+							<option value="date">времени</option>
+							<option value="price">стоимости</option>
+							<option value="duration">длительности</option>
+						</select>
+					</div>
+					<div className="row text-right ml-5 mr-3">показывать по:&nbsp;
                 <button className={classFilterChoiceFive}
-						onClick={this.filterChoiceTickets}>5
+							onClick={this.filterChoiceTickets}>5
                 </button>
-					<button className={classFilterChoiceTen}
-						onClick={this.filterChoiceTickets}>10
+						<button className={classFilterChoiceTen}
+							onClick={this.filterChoiceTickets}>10
                 </button>
-					<button className={classFilterChoiceTwenty}
-						onClick={this.filterChoiceTickets}>20
+						<button className={classFilterChoiceTwenty}
+							onClick={this.filterChoiceTickets}>20
                 </button>
+					</div>
+				</div>
+
+				{resultSearchTicketsJSX}
+
+				<div className="row justify-content-end mt-5">
+					<button className="page-search-select-number ml-3"
+						type="button"
+						onClick={this.setPrevPageOffset}
+						disabled={this.state.prevPageDisabled}>
+						<img src={iconSearchLeft} alt="иконка влево" />
+					</button>
+
+					{buttonsPages}
+
+					<button className="page-search-select-number ml-3"
+						type="button"
+						onClick={this.setNextPageOffset}
+						disabled={this.state.nextPageDisabled}>
+						<img src={iconSearchRight} alt="иконка вправо" />
+					</button>
 				</div>
 			</div>
-
-			{resultSearchTicketsJSX}
-
-			<div className="row justify-content-end mt-5">
-				<button className="page-search-select-number ml-3" 
-				type="button" 
-				onClick={this.setPrevPageOffset}
-				disabled={this.state.prevPageDisabled}>
-					<img src={iconSearchLeft} alt="иконка влево" />
-				</button>
-
-				{buttonsPages}
-
-				<button className="page-search-select-number ml-3" 
-				type="button" 
-				onClick={this.setNextPageOffset}
-				disabled={this.state.nextPageDisabled}>
-					<img src={iconSearchRight} alt="иконка вправо" />
-				</button>
-			</div>
-		</div>
-	);
+		);
 	}
 }
 
