f208dd30e321111a411590e8c88968c5eb7e1baa
mityaycho <mityaycho@gmail.com>
2020-04-24 15:42:24 +0300

    add some changes

--- src/api/api.js
+++ src/api/api.js
@@ -8,9 +8,9 @@ export const api = {
 	getLastRoutes() {
 		return instance.get('routes/last')
 	},
-	filterTicketsAndSeatsAPI(
+	filtersortTicketsAPI(
 		sectionSearch,
-		filterChoiceTicketsAndSeatsPages
+		sortTickets
 		// cityWhereFromId,
 		// cityWhereToId,
 		// sort,
@@ -37,28 +37,28 @@ export const api = {
 		// cityWhereToId: "5b9a2fa7f83e028786ea5673"
 		// `routes?from_city_id=${cityWhereFromId}&to_city_id=${cityWhereToId}` (line 28) old
 		// `routes?from_city_id=${sectionSearch.cityWhereFromId}&to_city_id=${sectionSearch.cityWhereToId}` (line 28)
-
+debugger
 		return instance.get(
 			`routes?from_city_id=${'5b9a2fa7f83e028786ea5672'}&to_city_id=${'5b9a2fa7f83e028786ea5673'}` +
-			(`${filterChoiceTicketsAndSeatsPages.sort}` ? `&sort=${filterChoiceTicketsAndSeatsPages.sort}` : '') +
-			(`${filterChoiceTicketsAndSeatsPages.limit}` ? `&limit=${filterChoiceTicketsAndSeatsPages.limit}` : '') +
-			(`${filterChoiceTicketsAndSeatsPages.offset}` ? `&offset=${filterChoiceTicketsAndSeatsPages.offset}` : '') +
-			`${filterChoiceTicketsAndSeatsPages.have_second_class ? '&have_second_class=true' : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.have_third_class ? '&have_third_class=true' : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.have_fourth_class ? '&have_fourth_class=true' : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.have_first_class ? '&have_first_class=true' : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.have_wifi ? '&have_wifi=true' : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.have_express ? '&have_express=true' : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.price_from ? `&price_from=${filterChoiceTicketsAndSeatsPages.price_from}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.price_to ? `&price_to=${filterChoiceTicketsAndSeatsPages.price_to}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.start_departure_hour_from ? `&start_departure_hour_from=${filterChoiceTicketsAndSeatsPages.start_departure_hour_from}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.start_departure_hour_to ? `&start_departure_hour_to=${filterChoiceTicketsAndSeatsPages.start_departure_hour_to}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.start_arrival_hour_from ? `&start_arrival_hour_from=${filterChoiceTicketsAndSeatsPages.start_arrival_hour_from}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.start_arrival_hour_to ? `&start_arrival_hour_to=${filterChoiceTicketsAndSeatsPages.start_arrival_hour_to}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.end_departure_hour_from ? `&end_departure_hour_from=${filterChoiceTicketsAndSeatsPages.end_departure_hour_from}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.end_departure_hour_to ? `&end_departure_hour_to=${filterChoiceTicketsAndSeatsPages.end_departure_hour_to}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.end_arrival_hour_from ? `&end_arrival_hour_from=${filterChoiceTicketsAndSeatsPages.end_arrival_hour_from}` : ''}` +
-			`${filterChoiceTicketsAndSeatsPages.end_arrival_hour_to ? `&end_arrival_hour_to=${filterChoiceTicketsAndSeatsPages.end_arrival_hour_to}` : ''}`
+			(`${sortTickets.sort}` ? `&sort=${sortTickets.sort}` : '') +
+			(`${sortTickets.limit}` ? `&limit=${sortTickets.limit}` : '') +
+			(`${sortTickets.offset}` ? `&offset=${sortTickets.offset}` : '') +
+			`${sortTickets.have_second_class ? '&have_second_class=true' : ''}` +
+			`${sortTickets.have_third_class ? '&have_third_class=true' : ''}` +
+			`${sortTickets.have_fourth_class ? '&have_fourth_class=true' : ''}` +
+			`${sortTickets.have_first_class ? '&have_first_class=true' : ''}` +
+			`${sortTickets.have_wifi ? '&have_wifi=true' : ''}` +
+			`${sortTickets.have_express ? '&have_express=true' : ''}` +
+			`${sortTickets.price_arr[0] ? `&price_from=${sortTickets.price_arr[0]}` : ''}` +
+			`${sortTickets.price_arr[1] ? `&price_to=${sortTickets.price_arr[1]}` : ''}` +
+			`${sortTickets.start_departure_hour_arr[0] ? `&start_departure_hour_from=${sortTickets.start_departure_hour_arr[0]}` : ''}` +
+			`${sortTickets.start_departure_hour_arr[1] ? `&start_departure_hour_to=${sortTickets.start_departure_hour_arr[1]}` : ''}` +
+			`${sortTickets.start_arrival_hour_arr[0] ? `&start_arrival_hour_from=${sortTickets.start_arrival_hour_arr[0]}` : ''}` +
+			`${sortTickets.start_arrival_hour_arr[1] ? `&start_arrival_hour_to=${sortTickets.start_arrival_hour_arr[1]}` : ''}` +
+			`${sortTickets.end_departure_hour_arr[0] ? `&end_departure_hour_from=${sortTickets.end_departure_hour_arr[0]}` : ''}` +
+			`${sortTickets.end_departure_hour_arr[1] ? `&end_departure_hour_to=${sortTickets.end_departure_hour_arr[1]}` : ''}` +
+			`${sortTickets.end_arrival_hour_arr[0] ? `&end_arrival_hour_from=${sortTickets.end_arrival_hour_arr[0]}` : ''}` +
+			`${sortTickets.end_arrival_hour_arr[1] ? `&end_arrival_hour_to=${sortTickets.end_arrival_hour_arr[1]}` : ''}`
 		)
 	},
 	setSeatSelection(trainId) {
--- src/redux/filterTicketsAndSeats-reducer.js
+++ src/redux/filterTicketsAndSeats-reducer.js
@@ -13,16 +13,11 @@ const initState = {
 	have_first_class: false,
 	have_wifi: false,
 	have_express: false,
-	price_from: 0,
-	price_to: 5000,
-	start_departure_hour_from: 0,
-	start_departure_hour_to: 24,
-	start_arrival_hour_from: 0,
-	start_arrival_hour_to: 24,
-	end_departure_hour_from: 0,
-	end_departure_hour_to: 24,
-	end_arrival_hour_from: 0,
-	end_arrival_hour_to: 24,
+	price_arr: [0, 5000],
+	start_departure_hour_arr: [0, 24],
+	start_arrival_hour_arr: [0, 24],
+	end_departure_hour_arr: [0, 24],
+	end_arrival_hour_arr: [0, 24],
 	customRangeCostFrom: false,
 	customRangeCostTo: false,
 	trainId: '',
@@ -50,6 +45,7 @@ export const filterTicketsAndSeatsReducerTC = (fieldName, fieldValue) => (dispat
 			)
 				.then(res => {
 					console.log(res)
+					debugger
 					dispatch(filterTicketsAndSeatsAC('ticketsArray', res.data.items));
 					dispatch(filterTicketsAndSeatsAC('totalCountTickets', res.data.total_count));
 				});
