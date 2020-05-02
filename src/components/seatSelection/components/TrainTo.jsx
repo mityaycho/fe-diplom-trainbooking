c183363f204a7856e47e3badc16cfbe7cc955dd4
mityaycho <mityaycho@gmail.com>
2020-05-02 21:33:42 +0300

    add logic to TrainTo

--- src/components/seatSelection/components/TrainTo.jsx
+++ src/components/seatSelection/components/TrainTo.jsx
@@ -3,11 +3,15 @@ import choiceTrainIcon from '../../../images/icon_choice_train.png';
 import choiceTimeIcon from '../../../images/icon_choice_time.png';
 import choiceOtherTrainButtonBack from '../../../images/choice_other_train_button_back.png';
 import iconSearchBack from '../../../images/icon_search_back.png';
+import TrainJSX from './TrainJSX';
+import train_fourth_class from '../../../images/train_fourth_class.png';
+import train_third_class from '../../../images/train_third_class.png';
+import train_second_class from '../../../images/train_second_class.png';
+import train_first_class from '../../../images/train_first_class.png';
+
 import { NavLink } from 'react-router-dom';
-import TrainFourthJSX from './TrainJSX';
-import TrainThirdJSX from './TrainThirdJSX';
-import TrainSecondJSX from './TrainSecondJSX';
-import TrainFirstJSX from './TrainFirstSX';
+import { connect } from 'react-redux';
+
 
 
 class TrainTo extends React.Component {
@@ -16,23 +20,51 @@ class TrainTo extends React.Component {
 		fourthClass: false,
 		thirdClass: false,
 		secondClass: false,
-		firstClass: false
+		firstClass: false,
+		fourth: [],
+		third: [],
+		second: [],
+		first: []
 	}
 
-	setFourthClass = () => this.setState({ fourthClass: true, thirdClass: false, secondClass: false, firstClass: false });
+	setFourthClass = () => {
+		this.props.choiceSeatsArray.map(el => {
+			if (el.coach.class_type === 'fourth') {
+				this.setState({ fourth: el, fourthClass: true, thirdClass: false, secondClass: false, firstClass: false });
+			}
+		});
+	}
 
-	setThirdClass = () => this.setState({ fourthClass: false, thirdClass: true, secondClass: false, firstClass: false });
+	setThirdClass = () => {
+		this.props.choiceSeatsArray.map(el => {
+			if (el.coach.class_type === 'third') {
+				this.setState({ third: el, fourthClass: false, thirdClass: true, secondClass: false, firstClass: false });
+			}
+		});
+	}
 
-	setSecondClass = () => this.setState({ fourthClass: false, thirdClass: false, secondClass: true, firstClass: false });
+	setSecondClass = () => {
+		this.props.choiceSeatsArray.map(el => {
+			if (el.coach.class_type === 'second') {
+				this.setState({ second: el, fourthClass: false, thirdClass: false, secondClass: true, firstClass: false });
+			}
+		});
+	}
 
-	setFirstClass = () => this.setState({ fourthClass: false, thirdClass: false, secondClass: false, firstClass: true });
+	setFirstClass = () => {
+		this.props.choiceSeatsArray.map(el => {
+			if (el.coach.class_type === 'first') {
+				this.setState({ first: el, fourthClass: false, thirdClass: false, secondClass: false, firstClass: true });
+			}
+		});
+	}
 
 	render() {
 
-		let fromDateTime = this.props.ticket.departure.from.datetime;
+		// let fromDateTime = this.props.ticket.departure.from.datetime;
 		let toDateTime = this.props.ticket.departure.to.datetime;
 		let duration = this.props.ticket.departure.duration;
-		let fromArrival = fromDateTime + duration;
+		// let fromArrival = fromDateTime + duration;
 		let toArrival = toDateTime + duration;
 
 		const getHours = (msc) => new Date(msc).getHours();
@@ -57,13 +89,13 @@ class TrainTo extends React.Component {
 					</div>
 					<div className="d-flex">
 						<ul className="list-unstyled align-self-center">
-							<li className="font-weight-bold">{getHours(toDateTime)}:{getMinutes(toDateTime)}</li>
+							<li className="font-weight-bold">{getHours(toArrival)}:{getMinutes(toArrival)}</li>
 							<li className="">{this.props.ticket.departure.to.city.name}</li>
 							<li className="font-weight-light">{this.props.ticket.departure.to.railway_station_name}</li>
 						</ul>
 						<img className="col align-self-center" src={iconSearchBack} alt="иконка стрелки вправо" />
 						<ul className="list-unstyled align-self-center">
-							<li className="font-weight-bold">{getHours(toArrival)}:{getMinutes(toArrival)}</li>
+							<li className="font-weight-bold">{getHours(toDateTime)}:{getMinutes(toDateTime)}</li>
 							<li className="">{this.props.ticket.departure.from.city.name}</li>
 							<li className="font-weight-light">{this.props.ticket.departure.from.railway_station_name}</li>
 						</ul>
@@ -91,30 +123,44 @@ class TrainTo extends React.Component {
 				<div className="horizontal-line-gray mt-5 mb-5"></div>
 				<h5 className="font-weight-bold ml-3">Тип вагона</h5>
 				<div className="choice-type-vagon-button d-flex justify-content-between mt-4 mb-2">
-					<button type="button" className="btn btn-outline-light ml-5" onClick={this.setFourthClass}>
+					<button type="button" className="btn btn-outline-light ml-5" 
+					onClick={this.setFourthClass}>
 						<div className="icon-type-vagon-seat align-self-center"></div>
 						<p>Сидячий</p>
 					</button>
-					<button type="button" className="btn btn-outline-light" onClick={this.setThirdClass}>
+					<button type="button" className="btn btn-outline-light" 
+					onClick={this.setThirdClass}>
 						<div className="icon-type-vagon-reserved-seat align-self-center"></div>
 						<p>Плацкарт</p>
 					</button>
-					<button type="button" className="btn btn-outline-light" onClick={this.setSecondClass}>
+					<button type="button" className="btn btn-outline-light" 
+					onClick={this.setSecondClass}>
 						<div className="icon-type-vagon-coupe align-self-center"></div>
 						<p>Купе</p>
 					</button>
-					<button type="button" className="btn btn-outline-light mr-5" onClick={this.setFirstClass}>
+					<button type="button" className="btn btn-outline-light mr-5" 
+					onClick={this.setFirstClass}>
 						<div className="icon-type-vagon-luxury align-self-center"></div>
 						<p>Люкс</p>
 					</button>
 				</div>
-				{this.state.fourthClass && <TrainFourthJSX />}
-				{this.state.thirdClass && <TrainThirdJSX />}
-				{this.state.secondClass && <TrainSecondJSX />}
-				{this.state.firstClass && <TrainFirstJSX />}
+				{this.state.fourthClass && <TrainJSX train={this.state.fourth} image={train_fourth_class} />}
+				{this.state.thirdClass && <TrainJSX train={this.state.third} image={train_third_class} />}
+				{this.state.secondClass && <TrainJSX train={this.state.second} image={train_second_class} />}
+				{this.state.firstClass && <TrainJSX train={this.state.first} image={train_first_class} />}
 			</div>
 		);
 	}
 }
 
-export default TrainTo;
+const mapStateToProps = (state) => {
+  return {
+		choiceSeatsArray: state.filterChoiceTicketsAndSeatsPages.choiceSeatsArray,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrainTo);
