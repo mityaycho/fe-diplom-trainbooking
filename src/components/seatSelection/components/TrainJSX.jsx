0b4c2b6a2d5de0f586b1ece65dad80428930ca5e
mityaycho <mityaycho@gmail.com>
2020-05-11 21:53:57 +0300

    add some changes

--- src/components/seatSelection/components/TrainJSX.jsx
+++ src/components/seatSelection/components/TrainJSX.jsx
@@ -9,13 +9,13 @@ const TrainJSX = (props) => {
 	const seatsMath = Math.ceil(props.train.coach.available_seats / 2);
 
 	const seatFirstRowJSX = props.train.seats.map(el => el.index <= 32 ?
-		<button className={`seats-index ${el.available}`} 
-		key={el.index} disabled={el.available} onClick={props.setSeatNumber}>{el.index}</button> : '');
+		<button className={`seats-index ${el.available}`}
+			key={el.index} disabled={el.available} onClick={props.setSeatNumber}>{el.index}</button> : '');
 
 	const seatTwoRowJSX = props.train.seats.map(el => el.index <= 32 ?
 		'' :
-		<button className={`seats-index two-row ${el.available}`} 
-		key={el.index} disabled={el.available} onClick={props.setSeatNumber}>{el.index}</button>);
+		<button className={`seats-index two-row ${el.available}`}
+			key={el.index} disabled={el.available} onClick={props.setSeatNumber}>{el.index}</button>);
 
 	return (
 		<div>
