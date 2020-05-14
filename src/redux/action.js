259ad587cdea7ca00744d2b43469d3c829305003
mityaycho <mityaycho@gmail.com>
2020-05-14 21:44:51 +0300

    add some changes

--- src/redux/action.js
+++ src/redux/action.js
@@ -4,23 +4,33 @@ export const SET_TICKETS_AND_SEATS = 'ticketsPayReducer/SET_TICKETS_AND_SEATS';
 export const SET_ROUTE_TRAIN_SEAT = 'orderTicketsReducer/SET_ROUTE_TRAIN_SEAT';
 
 
-export const setDataFormAC = (form) => ({ type: SET_FORM, form });
+export const setDataFormAC = (form) => ({
+	type: SET_FORM,
+	form
+});
 
-export const setLastRoutes = (lastRoutes) => ({ type: SET_LAST_ROUTES, lastRoutes });
+export const setLastRoutes = (lastRoutes) => ({
+	type: SET_LAST_ROUTES,
+	lastRoutes
+});
 
-export const filterTicketsAndSeatsAC = (fieldName, fieldValue) => ({type:SET_TICKETS_AND_SEATS, fieldName, fieldValue});
+export const filterTicketsAndSeatsAC = (fieldName, fieldValue) => ({
+	type: SET_TICKETS_AND_SEATS,
+	fieldName,
+	fieldValue
+});
 
 export const setRouteTrainSeatAC = (
-	route_direction_id, 
-	coach_id, 
-	seat_number, 
+	route_direction_id,
+	coach_id,
+	seat_number,
 	is_child,
 	include_children_seat
-) => ({ 
-	type: SET_ROUTE_TRAIN_SEAT, 
-	route_direction_id, 
-	coach_id, 
-	seat_number, 
+) => ({
+	type: SET_ROUTE_TRAIN_SEAT,
+	route_direction_id,
+	coach_id,
+	seat_number,
 	is_child,
 	include_children_seat
 });
