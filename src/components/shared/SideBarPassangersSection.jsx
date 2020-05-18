cadd19a78e9583622b67c2398915f15bcd1edb48
mityaycho <mityaycho@gmail.com>
2020-05-18 20:32:33 +0300

    add date and cities to SideBarPassangersSection

--- src/components/shared/SideBarPassangersSection.jsx
+++ src/components/shared/SideBarPassangersSection.jsx
@@ -13,6 +13,15 @@ import { connect } from 'react-redux';
 class SideBarPassangersSection extends React.Component {
 
 	render() {
+		const ticketSelected = this.props.ticketsArray.find(el => el.departure._id === this.props.trainId);
+		let fromDateTime = ticketSelected.departure.from.datetime;
+		let toDateTime = ticketSelected.departure.to.datetime;
+		let duration = ticketSelected.departure.duration;
+		let fromArrival = fromDateTime + duration;
+		let toArrival = toDateTime + duration;
+
+		const getHours = (msc) => new Date(msc).getHours();
+		const getMinutes = (msc) => (new Date(msc).getMinutes() < 10 ? '0' : '') + new Date(msc).getMinutes();
 
 		return (
 			<div className="trip-details-tickets col-lg-3 mt-5 mb-5">
@@ -21,8 +30,8 @@ class SideBarPassangersSection extends React.Component {
 					<hr className="bg-light mt-4" />
 					<div className="d-flex align-items-center">
 						<img className="ml-3" src={iconThere} alt="иконка туда" />
-						<h5 className="ml-2">Туда</h5>
-						<h6 className="ml-2 font-weight-light">30.08.2018</h6>
+						<h6 className="ml-1 pt-2">Туда</h6>
+						<h6 className="ml-auto pt-2 font-weight-light">{this.props.form.whereFromDate}</h6>
 						<img className="ml-auto mr-3" src={iconMinus} alt="..." />
 					</div>
 					<div className="d-flex justify-content-between">
@@ -31,34 +40,34 @@ class SideBarPassangersSection extends React.Component {
 							<p className="mt-3">Название</p>
 						</div>
 						<div className="mr-3 text-right">
-							<h6 className="mt-3">116С</h6>
+							<h6 className="mt-3">{ticketSelected.departure.train.name}</h6>
 							<p className="mt-3">Адлер</p>
-							<p className="mt-n3">Санкт-Петербург</p>
+							<p className="mt-n3">{ticketSelected.departure.from.city.name}</p>
 						</div>
 					</div>
 					<div className="d-flex justify-content-between">
 						<div className="ml-3 mt-3">
-							<h6>00:10</h6>
-							<p className="text-white-50">30.08.2018</p>
+							<h6>{getHours(fromDateTime)}:{getMinutes(fromDateTime)}</h6>
+							<p className="text-white-50">{this.props.form.whereFromDate}</p>
 						</div>
 						<div>
-							<p>9:42</p>
+							<p>{getHours(duration)}:{getMinutes(duration)}</p>
 							<img className="mt-n4" src={iconSearchThere} alt="иконка стрелки вправо" />
 						</div>
 						<div className="mr-3 mt-3 text-right">
-							<h6>9:52</h6>
-							<p className="text-white-50">31.08.2018</p>
+							<h6>{getHours(fromArrival)}:{getMinutes(fromArrival)}</h6>
+							<p className="text-white-50">{this.props.form.whereFromDate}</p>
 						</div>
 					</div>
 					<div className="d-flex justify-content-between">
 						<div className="ml-3">
-							<h6>Москва</h6>
-							<p className="text-white-50">Курский </p>
+							<h6>{ticketSelected.departure.from.city.name}</h6>
+							<p className="text-white-50">{ticketSelected.departure.from.railway_station_name}</p>
 							<p className="mt-n3 text-white-50">вокзал</p>
 						</div>
 						<div className="mr-3 text-right">
-							<h6>Санкт-Петербург</h6>
-							<p className="text-white-50">Ладожский</p>
+							<h6>{ticketSelected.departure.to.city.name}</h6>
+							<p className="text-white-50">{ticketSelected.departure.to.railway_station_name}</p>
 							<p className="mt-n3 text-white-50">вокзал</p>
 						</div>
 					</div>
@@ -66,8 +75,8 @@ class SideBarPassangersSection extends React.Component {
 
 					<div className="d-flex align-items-center">
 						<img className="ml-3" src={iconBack} alt="иконка туда" />
-						<h5 className="ml-2">Обратно</h5>
-						<h6 className="ml-2 font-weight-light">09.09.2018</h6>
+						<h6 className="ml-1 pt-2">Обратно</h6>
+						<h6 className="ml-auto pt-2 font-weight-light">{this.props.form.whereToDate}</h6>
 						<img className="ml-auto mr-3" src={iconMinus} alt="..." />
 					</div>
 					<div className="d-flex justify-content-between">
@@ -76,34 +85,34 @@ class SideBarPassangersSection extends React.Component {
 							<p className="mt-3">Название</p>
 						</div>
 						<div className="mr-3 text-right">
-							<h6 className="mt-3">116С</h6>
+							<h6 className="mt-3">{ticketSelected.departure.train.name}</h6>
 							<p className="mt-3">Адлер</p>
-							<p className="mt-n3">Санкт-Петербург</p>
+							<p className="mt-n3">{ticketSelected.departure.to.city.name}</p>
 						</div>
 					</div>
 					<div className="d-flex justify-content-between">
 						<div className="ml-3 mt-3">
-							<h6>00:10</h6>
-							<p className="text-white-50">09.09.2018</p>
+							<h6>{getHours(toDateTime)}:{getMinutes(toDateTime)}</h6>
+							<p className="text-white-50">{this.props.form.whereToDate}</p>
 						</div>
 						<div>
-							<p>9:42</p>
+							<p>{getHours(duration)}:{getMinutes(duration)}</p>
 							<img className="mt-n4" src={iconSearchThere} alt="иконка стрелки вправо" />
 						</div>
 						<div className="mr-3 mt-3 text-right">
-							<h6>9:52</h6>
-							<p className="text-white-50">08.09.2018</p>
+							<h6>{getHours(toArrival)}:{getMinutes(toArrival)}</h6>
+							<p className="text-white-50">{this.props.form.whereToDate}</p>
 						</div>
 					</div>
 					<div className="d-flex justify-content-between">
 						<div className="ml-3">
-							<h6>Москва</h6>
-							<p className="text-white-50">Курский </p>
+							<h6>{ticketSelected.departure.to.city.name}</h6>
+							<p className="text-white-50">{ticketSelected.departure.to.railway_station_name}</p>
 							<p className="mt-n3 text-white-50">вокзал</p>
 						</div>
 						<div className="mr-3 text-right">
-							<h6>Санкт-Петербург</h6>
-							<p className="text-white-50">Ладожский</p>
+							<h6>{ticketSelected.departure.from.city.name}</h6>
+							<p className="text-white-50">{ticketSelected.departure.from.railway_station_name}</p>
 							<p className="mt-n3 text-white-50">вокзал</p>
 						</div>
 					</div>
@@ -137,7 +146,10 @@ class SideBarPassangersSection extends React.Component {
 
 const mapStateToProps = (state) => {
 	return {
-		
+		form: state.sectionSearch.form,
+		ticketsArray: state.filterChoiceTicketsAndSeatsPages.ticketsArray,
+		choiceSeatsArray: state.filterChoiceTicketsAndSeatsPages.choiceSeatsArray,
+		trainId: state.filterChoiceTicketsAndSeatsPages.trainId
 	};
 };
 
