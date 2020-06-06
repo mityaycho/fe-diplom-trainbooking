0331a7814a118db43994f902556fa86bbb5c7a34
mityaycho <mityaycho@gmail.com>
2020-06-06 20:12:42 +0300

    add @media to ProgressLineCost

--- src/components/shared/ProgressLineCost.jsx
+++ src/components/shared/ProgressLineCost.jsx
@@ -4,28 +4,28 @@ import React from 'react';
 const ProgressLineCost = (props) => {
   return (
     <div className="progress-line-cost">
-      <ul className="list-group list-group-horizontal justify-content-between w-100 h-25 d-flex flex-wrap">
+      <ul className="d-flex list-group list-group-horizontal justify-content-between">
         <li
           className="progress-line-cost-button completed list-group-item w-25 d-flex font-weight-bold border-0">
           <p className="progress-line-cost-number ml-auto">1</p>
-          <p className="progress-line-cost-text mr-3">Билеты</p>
-          <img className="ml-5" src={props.tickets} alt="..."/>
+          <p className="progress-line-cost-text">Билеты</p>
+          <img className="ml-auto" src={props.tickets} alt="..."/>
         </li>
         <li
           className={`progress-line-cost-button ${props.passengersClass} list-group-item w-25 justify-content-center d-flex font-weight-bold border-0`}>
-          <p className="progress-line-cost-number">2</p>
-          <p className="progress-line-cost-text mr-5">Пассажиры</p>
-          <img src={props.passengers} alt="..."/>
+          <p className="progress-line-cost-number ml-auto">2</p>
+          <p className="progress-line-cost-text">Пассажиры</p>
+          <img className="ml-auto" src={props.passengers} alt="..."/>
         </li>
         <li
           className={`progress-line-cost-button ${props.paymentClass} list-group-item w-25 justify-content-center d-flex font-weight-bold border-0`}>
-          <p className="progress-line-cost-number">3</p>
-          <p className="progress-line-cost-text mr-5">Оплата</p>
-          <img src={props.payment} alt="..."/>
+          <p className="progress-line-cost-number ml-auto">3</p>
+          <p className="progress-line-cost-text">Оплата</p>
+          <img className="ml-auto" src={props.payment} alt="..."/>
         </li>
         <li
-          className={`progress-line-cost-button ${props.checkClass} list-group-item w-25 justify-content-left d-flex font-weight-bold border-0`}>
-          <p className="progress-line-cost-number ml-5">4</p>
+          className={`progress-line-cost-button ${props.checkClass} list-group-item w-25 justify-content-center d-flex font-weight-bold border-0`}>
+          <p className="progress-line-cost-number">4</p>
           <p className="progress-line-cost-text ">Проверка</p>
         </li>
       </ul>
